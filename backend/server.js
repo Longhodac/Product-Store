a02ab6145b00bff739aa2697632f52ee62e133f6
longhodac-ucsd <long110404@gmail.com>
2025-08-31 00:54:05 -0700

    added frontend

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,13 @@ import ProductRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.use("/api/products", ProductRoutes);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log(`Server started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
